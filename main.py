ff477c570c387d2c508ef64cade33c5eabf27aec
Prashant Raj <pr2125116@gmail.com>
2024-09-11 16:19:55 +0530

    Done

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
-menu = {"popcorn":20,
-        "apple":10,
-        "banana":10,
-        "toffee":5}
+# menu = {"popcorn":20,
+#         "apple":10,
+#         "banana":10,
+#         "toffee":5}
 
-cart =[]
-total =0
+# cart =[]
+# total =0
 
-print("-----Our Menu-----")
-for item, price in menu.items():
-    print(f"{item:10}: ${price:.2f}")
+# print("-----Our Menu-----")
+# for item, price in menu.items():
+#     print(f"{item:10}: ${price:.2f}")
 
-print("----------------")
+# print("----------------")
+
+# while True:
+#     food = input("Select an item (q to quit): ").lower()
+#     if food == "q":
+#         break
+#     elif menu.get(food) is not None:
+#         cart.append(food)
+
+# for food in cart:
+#     total += menu.get(food)
+#     print(food , end=" ")
+
+# print()
+# print(f"Your total is: {total:.2f}")
+
+#Random number guess
+
+import random
+
+low =1
+high = 100
+num = random.randint(1,100)
 
 while True:
-    food = input("Select an item (q to quit): ").lower()
-    if food == "q":
+    guess = input("Enter the guess number (999 to quit): ")
+    if guess == 999:
         break
-    elif menu.get(food) is not None:
-        cart.append(food)
-
-for food in cart:
-    total += menu.get(food)
-    print(food , end=" ")
-
-print()
-print(f"Your total is: {total:.2f}")
+    elif guess == num:
+        print("🤑")
+    else:
+        print("🥹")
+    
