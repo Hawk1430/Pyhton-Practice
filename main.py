e5d5e55f1043221f4488afe761b3513e086ee28c
Prashant Raj <pr2125116@gmail.com>
2024-09-11 12:10:14 +0530

    kkl

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-username = input("Enter username: ")
-length = len(username)
-if(len(username) <=12 and username.isalpha()):
-    print("Username accepted")
-else:
-    print("Username not accepted")
+menu = {"popcorn":20,
+        "apple":10,
+        "banana":10,
+        "toffee":5}
+
+cart =[]
+total =0
+
+print("-----Our Menu-----")
+for item, price in menu.items():
+    print(f"{item:10}: ${price:.2f}")
+
+print("----------------")
+
+while True:
+    food = input("Select an item (q to quit): ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+for food in cart:
+    total += menu.get(food)
+    print(food , end=" ")
+
+print()
+print(f"Your total is: {total:.2f}")
