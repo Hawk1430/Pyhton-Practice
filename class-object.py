a1267efb1c3c5207264a429bc1e148b932a45f3f
Prashant Raj <pr2125116@gmail.com>
2024-09-13 15:02:38 +0530

    classes and object

---
+++ class-object.py
@@ -0,0 +1,26 @@
+class Car:
+    def __init__(self,brand,year,color,forSale):
+        self.brand = brand 
+        self.year = year
+        self.color = color
+        self.forSale = forSale
+        
+    def drive(self):
+        print(f"You are driving {self.color} {self.brand}")
+    
+    def stop(self):
+        print(f"You are stopping {self.color} {self.brand}")
+        
+    def description(self):
+        print(f"The car is {self.brand} of {self.color} color of {self.year} and for sale its {self.forSale}")
+        if self.forSale:
+            print("Its for sale.")
+        else:
+            print("Its not for sale.")
+
+
+car1 = Car("BMW",2024,"blue",False)
+
+car1.drive()     
+car1.stop()
+car1.description()
