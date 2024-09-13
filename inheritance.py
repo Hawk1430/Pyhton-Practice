3551baf2a65358e66f880e03c04eed02f288a0d0
Prashant Raj <pr2125116@gmail.com>
2024-09-13 16:04:37 +0530

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,16 @@
+class Animal():
+    def __init__(self,name):
+        self.name = name
+        self.isAlive = True
+    
+    def walk(self):
+        print(f"{self.name} is walking")
+
+class Dog(Animal):
+    def voice(self):
+        print(f"{self.name} is barking")
+        
+dog = Dog("Jimmy")
+
+dog.walk()
+dog.voice()
