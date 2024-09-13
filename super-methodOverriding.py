1cea2e421326c60ebb900b2dd8c32f0adcc900f0
Prashant Raj <pr2125116@gmail.com>
2024-09-13 17:30:29 +0530

    super and method overriding

---
+++ super-methodOverriding.py
@@ -0,0 +1,22 @@
+class Shape:
+    def __init__(self,color,isFilled):
+        self.color = color
+        self.isFilled = isFilled
+        
+    def describe(self):
+        print(f"It is red and {"filled" if self.isFilled else "not filled"}.")
+        
+class Circle(Shape):
+    def __init__(self,color,isFilled,radius):
+        super().__init__(color,isFilled)
+        self.radius = radius
+        
+        # Method overriding
+    def describe(self):
+        print(f"It has an area of {3.14 * self.radius * self.radius} cm^2.")
+        super().describe()
+        
+circle = Circle("red", True,5)
+circle2 = Circle(color="Red",isFilled=True,radius=7)
+circle.describe()
+circle2.describe()
